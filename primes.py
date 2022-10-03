c224c5252d079444c4db25db19d2149ba73b88a2
mrwilliambarenga <williambarenga@gmail.com>
2022-10-03 10:38:31 +0100

    Complete Task

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    
+    if number_of_primes <= 0:
+        raise ValueError(f"Input = {number_of_primes} should have been greater than or equal to 1")
+
+    list = [2]
+    number = 3
+    count = 1
+
+    while count < number_of_primes:
+        
+        for x in list:
+
+            if number%x == 0:
+                break
+        else:
+            list.append(number)
+            count += 1
+
+        number += 1
+
+
     return list
